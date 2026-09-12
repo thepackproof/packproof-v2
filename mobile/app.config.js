88b03067a371cb7bbf41aa9cc209cfc04b8ca0d9
PackProof™ <nericollin@thepackproof.com>
2026-09-11 22:53:32 -0400

    Bump Android test build to 0.3.18 (47)

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -40,7 +40,7 @@ module.exports = {
     name: isCameraSpike ? "PackProof Camera Test" : "PackProof",
     slug: "packproof",
     owner: "packproof-llc",
-    version: "0.3.10",
+    version: "0.3.18",
     orientation: "portrait",
     userInterfaceStyle: "automatic",
     androidStatusBar: {
@@ -63,7 +63,7 @@ module.exports = {
     },
     android: {
       package: isCameraSpike ? "com.packproof.mobile.cameraspike" : "com.packproof.mobile",
-      versionCode: 39,
+      versionCode: 47,
       allowBackup: false,
       usesCleartextTraffic: !isPlayRelease,
       adaptiveIcon: {
